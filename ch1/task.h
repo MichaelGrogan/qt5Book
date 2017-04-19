9cb54f8b2b0c3500e7dfb7e9bb7d0c0ba72e68c8
Michael Grogan <michael.grogan@wontok.com>
2017-04-19 11:10:07 +1000

    added ch1

---
+++ ch1/task.h
@@ -0,0 +1,34 @@
+#ifndef TASK_H
+#define TASK_H
+
+#include <QWidget>
+#include <QString>
+
+namespace Ui {
+class Task;
+}
+
+class Task : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Task(const QString& name, QWidget *parent = 0);
+    ~Task();
+
+    void setName(const QString& name);
+    QString name() const;
+    bool isCompleted() const;
+
+public slots:
+    void rename();
+    void checked(bool checked);
+signals:
+    void removed(Task* task);
+    void statusChanged(Task* task);
+
+private:
+    Ui::Task *ui;
+};
+
+#endif // TASK_H
